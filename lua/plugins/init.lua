40e6569eaf5c7f61647381a9069eeebec5c0a411
Subodh Singh <singh.01.subodh@gmail.com>
2025-03-16 13:31:29 +0530

    Add colorscheme
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,19 @@
 return {
+    { -- colorscheme
+        'folke/tokyonight.nvim',
+        priority = 1000, -- Make sure to load this before all the other start plugins.
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require('tokyonight').setup {
+              styles = {
+                comments = { italic = false }, -- Disable italics in comments
+              },
+            }
+            -- 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+            vim.cmd.colorscheme 'tokyonight-night'
+        end,
+    },
+
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
     { -- Adds git related signs to the gutter, as well as utilities for managing changes
