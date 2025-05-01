caaef01bae2bd436d7234280a678336839100f56
Subodh Singh <singh.01.subodh@gmail.com>
2025-05-02 01:14:37 +0530

    Add keybind for grepping git files
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,18 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader><leader>', builtin.git_files, { desc = 'Telescope find git files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 
         vim.keymap.set('n', '<leader>/', function()
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-              winblend = 10,
-              previewer = false,
+                winblend = 10,
+                previewer = false,
             })
         end, { desc = '[/] Fuzzily search in current buffer' })
     end,
